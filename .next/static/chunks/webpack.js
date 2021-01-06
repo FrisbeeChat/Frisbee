9d220dca838271a5584bcb3002321676c2c267bf
Blake-Whitham <bwhitham77@gmail.com>
2021-01-05 23:56:25 -0800

    1-5-21 final commit of the night

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "33ef5963c2cb0e654d29";
+/******/ 	var hotCurrentHash = "3edab0490223f5da118b";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- pages/api/changeSettings.ts
+++ pages/api/changeSettings.ts
@@ -1,23 +1,23 @@
-// import type { NextApiRequest, NextApiResponse } from 'next';
-// import db from '../../database/connect/db';
-// import model from '../../database/model/UserModel';
-// import { User } from '../../database/model/UserModel';
-// import { authorize } from './authMiddleware';
+import type { NextApiRequest, NextApiResponse } from 'next';
+import db from '../../database/connect/db';
+import model from '../../database/model/UserModel';
+import { User } from '../../database/model/UserModel';
+import { authorize } from './authMiddleware';
 
-// export const changeSettings = (req: NextApiRequest, res: NextApiResponse) => {
-//   model.changeSettings(req.body, (err: Error | null, results?: User[] | null) => {
-//     if (err) {
-//       res.status(400).end();
-//     } else {
-//       res.json(results);
-//     }
-//   })
-// };
+export const changeSettings = (req: NextApiRequest, res: NextApiResponse) => {
+  // model.changeSettings(req.body, (err: Error | null, results?: User[] | null) => {
+  //   if (err) {
+  //     res.status(400).end();
+  //   } else {
+  //     res.json(results);
+  //   }
+  // })
+};
 
-// export const config = {
-//   api: {
-//     externalResolver: true,
-//   },
-// }
+export const config = {
+  api: {
+    externalResolver: true,
+  },
+}
 
-// export default authorize(changeSettings);
+export default authorize(changeSettings);
